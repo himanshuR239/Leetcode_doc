f894816bc6a1475218dca180c8baa5524eadfca8
himanshuR239 <himanshurajpoot2309@gmail.com>
2026-07-29 14:08:52 +0530

    Time: 0 ms (100%), Space: 8.2 MB (25.23%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        int n = s.size();
+
+        long long size = 0;
+
+        for(int i = 0; i < n; i++){
+            if(isdigit(s[i])){
+                size *= (s[i] - '0');
+            }
+            else size++;
+        }
+
+        for(int i = n-1; i >= 0; i--){
+            k %= size;
+
+            if(k == 0 && isalpha(s[i])){
+                return string(1, s[i]);
+            }
+
+            if(isdigit(s[i])){
+                size /= (s[i] - '0');
+            }
+            else size--;
+        }
+
+        return "";
+    }
+};
