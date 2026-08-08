7bb7a1da3d3a96358fc7d9d9e7dcf5408d7eea1b
himanshuR239 <himanshurajpoot2309@gmail.com>
2026-08-08 11:18:15 +0530

    Time: 314 ms (24.02%), Space: 15.9 MB (34.23%) - LeetHub

---
+++ 1931-painting-a-grid-with-three-different-colors/1931-painting-a-grid-with-three-different-colors.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<string> columnStates;
+    int M = 1e9+7;
+    vector<vector<int>> dp;
+
+    void generateColumnStates(string curr, char prevChar, int l, int m){
+        if(l == m){
+            columnStates.push_back(curr);
+            return;
+        }
+
+        for(char ch : {'R', 'G', 'B'}){
+            if(ch == prevChar) continue;
+
+            generateColumnStates(curr + ch, ch, l+1, m);
+        }
+    }
+
+    int solve(int remainCols, int prevIdx, int m){
+        if(remainCols == 0) return 1;
+
+        if(dp[remainCols][prevIdx] != -1) return dp[remainCols][prevIdx];
+
+        int ways = 0;
+        string prevState = columnStates[prevIdx];
+
+        for(int i = 0; i < columnStates.size(); i++){
+            if(i == prevIdx) continue;
+
+            string currState = columnStates[i];
+            bool valid = true;
+
+            for(int j = 0; j < m; j++){
+                if(prevState[j] == currState[j]){
+                    valid = false;
+                    break;
+                }
+            }
+
+            if(valid){
+                ways = (ways + solve(remainCols-1, i, m)) % M;
+            }
+        }
+
+        return dp[remainCols][prevIdx] = ways;
+    }
+
+    int colorTheGrid(int m, int n) {
+        generateColumnStates("", '#', 0, m);
+        int totalStates = columnStates.size();
+
+        dp = vector<vector<int>>(n+1, vector<int>(totalStates+1, -1));
+
+        int result = 0;
+        for(int i = 0; i < columnStates.size(); i++){
+            result = (result + solve(n-1, i, m)) % M;
+        }
+
+        return result;
+    }
+};
