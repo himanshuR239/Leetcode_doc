d02938a66b37569940e912caf319670557816148
himanshuR239 <himanshurajpoot2309@gmail.com>
2026-08-08 11:42:19 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.9%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -1,63 +1,15 @@
 class Solution {
 public:
-    vector<string> columnStates;
-    int M = 1e9+7;
-    vector<vector<int>> dp;
-
-    void generateColumnStates(string curr, char prevChar, int l, int m){
-        if(l == m){
-            columnStates.push_back(curr);
-            return;
-        }
-
-        for(char ch : {'R', 'G', 'B'}){
-            if(ch == prevChar) continue;
-
-            generateColumnStates(curr + ch, ch, l+1, m);
-        }
-    }
-
-    int solve(int remainCols, int prevIdx, int m){
-        if(remainCols == 0) return 1;
-
-        if(dp[remainCols][prevIdx] != -1) return dp[remainCols][prevIdx];
-
-        int ways = 0;
-        string prevState = columnStates[prevIdx];
-
-        for(int i = 0; i < columnStates.size(); i++){
-            if(i == prevIdx) continue;
-
-            string currState = columnStates[i];
-            bool valid = true;
-
-            for(int j = 0; j < m; j++){
-                if(prevState[j] == currState[j]){
-                    valid = false;
-                    break;
-                }
-            }
-
-            if(valid){
-                ways = (ways + solve(remainCols-1, i, m)) % M;
-            }
-        }
-
-        return dp[remainCols][prevIdx] = ways;
-    }
-
+    const int mod = 1e9+7;
     int numOfWays(int n) {
-        int m = 3;
-        generateColumnStates("", '#', 0, m);
-        int totalStates = columnStates.size();
-
-        dp = vector<vector<int>>(n+1, vector<int>(totalStates+1, -1));
-
-        int result = 0;
-        for(int i = 0; i < columnStates.size(); i++){
-            result = (result + solve(n-1, i, m)) % M;
+        long long aba = 6;
+        long long abc = 6;
+        for(int i=1 ;i<n ;i++){
+            long long naba = (1LL*3*aba + 1LL*2*abc)%mod;
+            long long nabc = (1LL*2*aba + 1LL*2*abc)%mod;
+            aba = naba;
+            abc = nabc;
         }
-
-        return result;
+        return (aba+abc)%mod;
     }
 };
