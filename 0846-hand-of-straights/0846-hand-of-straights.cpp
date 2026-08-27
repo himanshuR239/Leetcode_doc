6cf561352a82a5e89e0fde8ab6866e2395c0afa2
himanshuR239 <himanshurajpoot2309@gmail.com>
2026-08-27 16:40:07 +0530

    Time: 23 ms (89.09%), Space: 31.1 MB (80.02%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int gs) {
+        int n = hand.size();
+
+        if(n % gs != 0) return false;
+
+        sort(hand.begin(), hand.end());
+        unordered_map<int, int> mpp;
+        for(int num : hand) mpp[num]++;
+        for(int i = 0; i < n; i++){
+            int num = hand[i];
+            if(mpp.find(num) == mpp.end()) continue;;
+            for(int j = 0; j < gs; j++){
+                if(mpp.find(num) == mpp.end()) return false;
+                else{
+                    mpp[num]--;
+                    if(mpp[num]==0) mpp.erase(num);
+                }
+                num++;
+            }
+        }
+
+        return true;
+    }
+};
