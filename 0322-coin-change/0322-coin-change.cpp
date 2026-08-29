e7dea1155506aaef601ae6cb254930aeaa047580
himanshuR239 <himanshurajpoot2309@gmail.com>
2026-08-29 18:32:02 +0530

    Time: 52 ms (22.37%), Space: 48.8 MB (45.6%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
-    int n;
-    int dp[13][10001];
-    int solve(int ind, vector<int>& coins, int amount){
-        if(ind >= n) return 1e9;
-
-        if(amount == 0) return 0;
-
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-
-        int notTake = solve(ind+1, coins, amount);
-        int take = INT_MAX;
-        if(coins[ind] <= amount){
-            take = 1 + solve(ind, coins, amount - coins[ind]);
-        }
-
-        return dp[ind][amount] = min(take, notTake);
-    }
+    // int f(int ind, vector<int>& coins, int amount, vector<vector<int>>& dp){
+    //     if(ind == 0){
+    //         if(amount % coins[0] == 0) return amount/coins[0];
+    //         else return 1e9;
+    //     }
+    //     if(dp[ind][amount] != -1) return dp[ind][amount];
+    //     int notTake = 0 + f(ind-1, coins, amount, dp);
+    //     int take = INT_MAX;
+    //     if(coins[ind] <= amount) take = 1 + f(ind, coins, amount-coins[ind], dp);
+    //     return dp[ind][amount] = min(notTake, take);
+    // }
 
     int coinChange(vector<int>& coins, int amount) {
-        n = coins.size();
-        memset(dp, -1, sizeof(dp));
-        int ans = solve(0, coins, amount);
-        return ans == 1e9 ? -1 : ans;
+        int n = coins.size();
+        vector<vector<int>>dp(n, vector<int>(amount+1, 0));
+        for(int a=0;a<=amount;a++){
+            if(a % coins[0] == 0) dp[0][a] = a/coins[0];
+            else dp[0][a] = 1e9;
+        }
+        for(int ind = 1;ind<n;ind++){
+            for(int tar=0;tar<=amount;tar++){
+                int notTake = 0 + dp[ind-1][tar];
+                int take = INT_MAX;
+                if(coins[ind] <= tar) take = 1 + dp[ind][tar-coins[ind]];
+                dp[ind][tar] = min(notTake, take);
+            }
+        }
+
+        int ans = dp[n-1][amount];
+        if(ans >= 1e9) return -1;
+        return ans;
     }
 };
